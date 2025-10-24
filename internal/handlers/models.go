14930d4c938aedb0c715ba4ac37498d4d4b2e5e7
Abdallah Alathamneh <abdallah.alathamneh@mazad.bh>
2025-10-24 13:30:49 +0300

    transaction & user id relationship

--- internal/handlers/models.go
+++ internal/handlers/models.go
@@ -3,5 +3,6 @@ package handlers
 // Transaction represents a row in the PostgreSQL 'transaction' table.
 type Transaction struct {
 	ID     int     `json:"id"`
-	Amount float64 `json:"amount"` // Use float64 for monetary values in Go
+	Amount float64 `json:"amount"`  // Use float64 for monetary values in Go
+	UserID int     `json:"user_id"` // Foreign key to users table
 }
